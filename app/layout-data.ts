421fd742c2d849cd1b281a86e1c7eda70bd78780
nextcms-udemy <nextcms@hotmail.com>
2025-01-22 23:42:05 +0300

    test

---
+++ app/layout-data.ts
@@ -0,0 +1,72 @@
+export const layoutData = [
+  {
+    id: 1,
+    content: ` 
+            <span class="text-span">
+              File name should be exactly <span>layout.tsx</span>
+            </span>`,
+  },
+  {
+    id: 2,
+    content: ` 
+            <span>
+              Html and body tags should only be in the
+              <span>root layout file</span>
+            </span>`,
+  },
+  {
+    id: 3,
+    content: `
+            <span>
+              Should be <span>export default function.</span>
+            </span>`,
+  },
+  {
+    id: 4,
+    content: ` 
+            <span>
+              Like all components and pages, layout names{" "}
+              <span>must start with a capital</span> letter.
+              <div className="flex-1 flex-col flex">
+                <p>For example;</p>
+                <span>
+                  <ArrowRight />
+                  RootLayout
+                </span>
+                <span>
+                  {" "}
+                  <ArrowRight />
+                  Layout
+                </span>
+                <span>
+                  {" "}
+                  <ArrowRight />
+                  DashboardLayout
+                </span>
+              </div>
+            </span>`,
+  },
+  {
+    id: 5,
+    content: `
+            <span>
+              Must contain <span>children</span> prop to show child pages
+            </span>`,
+  },
+  {
+    id: 6,
+    content: ``,
+  },
+  {
+    id: 7,
+    content: ``,
+  },
+  {
+    id: 8,
+    content: ``,
+  },
+  {
+    id: 9,
+    content: ``,
+  },
+]
